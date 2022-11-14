ac5b664ac2cb37c3ef143e48aa0d202a72feb7e1
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-11-14 22:29:53 +0100

    init v2

---
+++ assets/js/theme.js
@@ -0,0 +1,26 @@
+function loadTheme(){
+	if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+		document.documentElement.classList.add('dark')
+		if(document.getElementById('darkModeChanger')){
+			document.getElementById('darkModeChanger').innerHTML = '<i class="fa-duotone fa-sun-bright"></i>'
+		}else setTimeout(loadTheme, 1000)
+	} else {
+		document.documentElement.classList.remove('dark')
+		if(document.getElementById('darkModeChanger')){
+			document.getElementById('darkModeChanger').innerHTML = '<i class="fa-duotone fa-moon"></i>'
+		}else setTimeout(loadTheme, 1000)
+	}
+}
+
+function toggleDarkMode(auto=false) {
+	if(auto){
+		localStorage.theme = ''
+	}else if (document.documentElement.classList.contains('dark')) {
+		localStorage.theme = 'light'
+	} else {
+		localStorage.theme = 'dark'
+	}
+	loadTheme()
+}
+
+loadTheme()
