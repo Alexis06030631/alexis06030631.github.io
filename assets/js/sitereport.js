ef7019f6eaceb3b335be6ad8b0ee14ae9afa252f
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-11-14 22:29:02 +0100

    prepare for v2

--- assets/js/sitereport.js
+++
@@ -1,9 +0,0 @@
-addEventListener('DOMContentLoaded', () => {
-    for (const summary of document.querySelectorAll('.page[data-path]>:first-child')) {
-        const anchor = summary.querySelector('a'),
-            href = ' ' + location.origin + summary.parentNode.dataset.path;
-
-        anchor.innerText = href;
-        anchor.href = href;
-    }
-});
